2504a99304cc6bc9a4c5a1fe79fe32032b4156e7
PG22Arseniy <pg22arseniy@vfs.com>
2023-03-15 08:17:51 -0700

    Some more products and their images

--- ecommerce-project/src/utils/data.ts
+++ ecommerce-project/src/utils/data.ts
@@ -13,7 +13,7 @@ type ProductProps = {
 type DataProps = {
     products: ProductProps[]
 }
-const data:DataProps = {
+export const data:DataProps = { 
     products: [
         {
             name: "Red Shirt",
@@ -23,6 +23,33 @@ const data:DataProps = {
             numReviews: 0,
             countInStock: 10,
             description: "A red Shirt"
+        },
+        {
+            name: "White Shirt",
+            slug: "white-shirt",
+            category: "Shirts",
+            imageDir: "/images/shirt2.jpg",
+            numReviews: 0,
+            countInStock: 5,
+            description: "A white Shirt"
+        },
+        {
+            name: "Black Shirt",
+            slug: "vlack-shirt",
+            category: "Shirts",
+            imageDir: "/images/shirt3.jpg",
+            numReviews: 0,
+            countInStock: 12,
+            description: "A black Shirt"
+        },
+        {
+            name: "Yellow Shirt",
+            slug: "yellow-shirt",
+            category: "Shirts",
+            imageDir: "/images/shirt4.jpg",
+            numReviews: 0,
+            countInStock: 7,
+            description: "A yellow Shirt" 
         }
     ]
 }
